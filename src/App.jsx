345facae4750ee1a8e5f9a90b75d2e7db4b9f912
Nobiyyan <mrayyan.firmansyah@gmail.com>
2026-02-09 21:49:48 +0700

    fix smthn

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import Header from "./Header.jsx";
 import Hero from "./Hero.jsx";
 import "./App.css";
-import ProductCatalog from "./productCatalog.jsx";
+import ProductCatalog from "./ProductCatalog.jsx";
 import Footer from "./Footer.jsx";
 
 function App() {
